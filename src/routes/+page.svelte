53ff92097d18072642a9e3456ec65478a165ccce
b-sharman <nonoesimposible72@gmail.com>
2024-01-20 14:04:13 -0700

    restore previous page into svelte project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import '../global.css';
+</script>
+
+<main>
+  <h1>Byron Sharman</h1>
+  <p>I'm Byron, a computer science student at Mines.</p>
+  <ul>
+    <li><a href="https://github.com/b-sharman">GitHub</a></li>
+    <li><a href="https://www.linkedin.com/in/byron-sharman/">LinkedIn</a></li>
+  </ul>
+</main>
