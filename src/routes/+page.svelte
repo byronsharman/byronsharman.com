1f178cf26e9b0abe56cf2fcd8ac34b0513e2cc47
b-sharman <nonoesimposible72@gmail.com>
2024-01-20 16:43:41 -0700

    inject email upon page load
    bit of an annoying flash, but I think it's an acceptable compromise for
    now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
-<script>
+<script lang='ts'>
   import '../global.css';
+  import Email from './email.svelte';
 </script>
 
 <main>
   <h1>Byron Sharman</h1>
   <p>I'm Byron, a computer science student at Mines.</p>
   <ul>
+    <li><Email /></li>
     <li><a href="https://github.com/b-sharman">GitHub</a></li>
     <li><a href="https://www.linkedin.com/in/byron-sharman/">LinkedIn</a></li>
   </ul>
---
+++ src/routes/email.svelte
@@ -0,0 +1,24 @@
+<script lang='ts'>
+  import { onMount } from 'svelte';
+
+  function decodeRot14(s: string): string {
+    const regex = /[a-z]/;
+    return s.split('')
+      .map(
+        (char) => {
+          if (regex.test(char)) {
+            return String.fromCharCode(char.charCodeAt(0) + (char < 'm' ? 14 : -12));
+          }
+          else { return char; }
+        }
+      )
+      .join('');
+  }
+
+  let ea: string = '';
+  onMount(() => {
+    ea = decodeRot14(atob('bmtkYXouei5ldG1keW16QHN5bXV4Lm9heQ=='));
+  });
+</script>
+
+<a href={`mailto:${ea}`}>{ea}</a>
