3e9e19f39a39a5ca59b42fea1f6103670277790a
b-sharman <nonoesimposible72@gmail.com>
2024-01-20 22:16:23 -0700

    make it somewhat presentable

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,23 @@
   import Email from './email.svelte';
 </script>
 
-<main>
-  <h1>Byron Sharman</h1>
-  <p>I'm Byron, a computer science student at Mines.</p>
-  <ul>
-    <li><Email /></li>
-    <li><a href="https://github.com/b-sharman">GitHub</a></li>
-    <li><a href="https://www.linkedin.com/in/byron-sharman/">LinkedIn</a></li>
-  </ul>
-</main>
+<div id="outermost-flex">
+  <main>
+    <h1>Byron Sharman</h1>
+    <p>
+      I'm Byron, a computer science student at Mines. Of course, I really like to
+      write code, learn new technologies, and (sometimes) even un-break my Arch
+      install.
+    </p>
+    <p>
+      I am also obsessed with the musical compositions of this old Russian guy
+      named Sergei Rachmaninoff. My favorite piece changes every week or so, but
+      right now, it's the original 1926 version of his Piano Concerto No.&nbsp;4.
+    </p>
+    <ul>
+      <li><Email /></li>
+      <li><a href="https://github.com/b-sharman">GitHub</a></li>
+      <li><a href="https://www.linkedin.com/in/byron-sharman/">LinkedIn</a></li>
+    </ul>
+  </main>
+</div>
