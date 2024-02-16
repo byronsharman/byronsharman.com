7a07a43450ebe3ec36fdbec7b085a90ca9c28b0a
b-sharman <byron.n.sharman@gmail.com>
2024-02-16 09:07:53 -0700

    first step: render Markdown from hardcoded string

---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang='ts'>
+  import '../../../output.css';
+  import type { PageData } from './$types';
+
+  export let data: PageData;
+</script>
+
+<div class="flex justify-center p-4">
+  <main class="max-w-prose">
+    <article class="my-6 prose lg:prose-xl">
+      {@html data[data.slug]}
+    </article>
+  </main>
+</div>
---
+++ src/routes/blog/[slug]/+page.ts
@@ -0,0 +1,21 @@
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from './$types';
+import { marked } from 'marked'
+
+const lorem = `# Lorem Ipsum
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim aeque doleamus animo, cum corpore dolemus, fieri tamen permagna accessio potest, si aliquod aeternum et infinitum impendere malum nobis opinemur. Quod.
+
+Ullus investigandi veri, nisi inveneris, et quaerendi defatigatio turpis est, cum esset accusata et vituperata ab Hortensio. Qui liber cum et mortem contemnit, qua qui est imbutus quietus esse numquam potest. Praeterea bona praeterita grata recordatione.
+
+Ullus investigandi veri, nisi inveneris, et quaerendi defatigatio turpis est, cum esset accusata et vituperata ab Hortensio. Qui liber cum et mortem contemnit, qua qui est imbutus quietus esse numquam potest. Praeterea bona praeterita grata recordatione renovata delectant. Est autem situm in nobis ut et voluptates et dolores nasci fatemur e corporis voluptatibus et doloribus -- itaque concedo, quod modo dicebas, cadere causa, si.`;
+
+export const load: PageLoad = ({ params }) => {
+  let retval = {'slug': params.slug};
+  if (params.slug === 'lorem') {
+    retval.lorem = marked(lorem);
+    return retval;
+  }
+
+  error(404, 'Not found');
+};
