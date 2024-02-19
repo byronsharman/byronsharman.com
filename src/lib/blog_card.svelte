b08b8ea0ea492b26528bb6e4fab5fd73b3c5465e
b-sharman <byron.n.sharman@gmail.com>
2024-02-19 09:58:00 -0700

    restore mobile-friendliness

--- src/lib/blog_card.svelte
+++ src/lib/blog_card.svelte
@@ -9,7 +9,7 @@
 <li class="border hover:border-gray-400 rounded-xl my-4">
   <a href={`/blog/${blog.slug}`} class="block p-4 rounded-lg">
     <h3 class="font-bold text-xl mb-4">{blog.title}</h3>
-    <p class="truncate">{blog.preview}</p>
+    <p class="line-clamp-3">{blog.preview}</p>
     <p class="mt-2 text-sm text-gray-600">{new Date(blog.date * 1000 - tzOffset).toISOString().slice(0,10)}</p>
   </a>
 </li>
