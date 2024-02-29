aa9f8e9564ef2ae449c0f3c72e847e1fd88ef885
b-sharman <byron.n.sharman@gmail.com>
2024-02-29 10:46:59 -0700

    all-around image improvements
    Modified the renderer so that images are now wrapped in <figure>s, and
    titles are now put into <figcaption>s. This allows me to center them
    pretty easily.

--- src/routes/blog/[slug]/+page.server.ts
+++ src/routes/blog/[slug]/+page.server.ts
@@ -2,6 +2,22 @@ import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 import { marked } from 'marked';
 
+const renderer = {
+  // this is just the defualt renderer
+  // https://github.com/markedjs/marked/blob/master/src/Renderer.ts#L135-L148
+  // but modified to wrap everything in a <figure> and use titles as captions
+  image(href: string, title: string | null, text: string): string {
+    if (href === "") return text;
+    let out = `<figure class="flex flex-col text-center"><img src="${href}" alt="${text}" class="mx-auto" />`;
+    if (title) {
+      out += `<figcaption>${title}</figcaption>`;
+    }
+    out += '</figure>';
+    return out;
+  }
+}
+marked.use({ renderer });
+
 export const load: PageLoad = async (p) => {
   const blogs = await p.fetch('/blog/index.json')
     .then(res => res.json());
