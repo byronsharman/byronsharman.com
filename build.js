d3a296de8a47df5c067d72bb26ef346c03a1503f
b-sharman <byron.n.sharman@gmail.com>
2024-03-01 11:53:17 -0700

    add RSS feed

--- build.js
+++ build.js
@@ -1,11 +1,38 @@
 // my first time copy and pasting a script from ChatGPT :P
 
+import { readFileSync, writeFileSync } from 'fs';
 import fs from 'fs-extra';
 import { build } from 'vite';
 
 async function runBuild() {
   try {
-    // Process your assets here before building
+    console.log('Writing RSS file...');
+    let xml_output = `
+<rss xmlns:atom="http://www.w3.org/2005/Atom" version="2.0">
+  <channel>
+    <title>Byron Sharman's blog</title>
+    <link>https://b-sharman.dev/</link>
+    <description>The latest blog posts from Byron Sharman</description>
+    <language>en-us</language>
+    <atom:link href="https://b-sharman.dev/blog.xml" rel="self" type="application/rss+xml"/>
+`;
+    const data = readFileSync('static/blog/build/index.json');
+    Object.values(JSON.parse(data)).forEach((blog) => {
+      xml_output += `    <item>
+      <title>${blog.title}</title>
+      <description>${blog.preview}</description>
+      <link>https://b-sharman.dev/blog/${blog.slug}</link>
+      <pubDate>${new Date(blog.date * 1000).toUTCString()}</pubDate>
+    </item>
+`;
+    });
+    xml_output += `
+  </channel>
+</rss>
+`;
+    writeFileSync('build/blog.xml', xml_output);
+    // needed to stop Vite from complaining about not finding blog.xml
+    writeFileSync('static/blog.xml', xml_output);
 
     // Run Vite build
     console.log('Building Svelte project with Vite...');
