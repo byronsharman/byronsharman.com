e652c9fdd764b5a0bb3f30f124699b190dcbecd4
b-sharman <byron.n.sharman@gmail.com>
2024-03-07 20:52:56 -0700

    add webring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -57,5 +57,19 @@
       </ul>
     </section>
 
+    <div class="flex flex-row justify-between text-gray-500 text-xs">
+      <a href="https://grantlemons.com/webring/prev">
+        <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M4.205 8.72805L12.205 3.72805C13.2041 3.10363 14.5 3.82189 14.5 5.00004V15C14.5 16.1782 13.2041 16.8965 12.205 16.272L4.205 11.272C3.265 10.6845 3.265 9.31555 4.205 8.72805Z" />
+        </svg>
+      </a>
+      <p class="mx-8">Come here from one of my friends' sites? Use these buttons to navigate the webring.</p>
+      <a href="https://grantlemons.com/webring/next">
+        <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M15.795 11.272L7.795 16.272C6.79593 16.8964 5.5 16.1782 5.5 15L5.5 5.00002C5.5 3.82186 6.79593 3.1036 7.795 3.72802L15.795 8.72802C16.735 9.31552 16.735 10.6845 15.795 11.272Z" />
+        </svg>
+      </a>
+    </div>
+
   </main>
 </div>
