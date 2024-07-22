5c6509c8b4a7c8023e64d1e3ea28b57d3f15512a
b-sharman <byron.n.sharman@gmail.com>
2024-07-22 20:48:21 +1200

    re-stylize project cards

--- src/lib/project_card.svelte
+++ src/lib/project_card.svelte
@@ -3,12 +3,25 @@
   export let project: Project;
 </script>
 
-<li class="border hover:border-gray-400 rounded-xl my-4">
-  <a href={project.url} class="block p-4 rounded-lg">
-    <h3 class="font-bold text-xl mb-2">{project.name}</h3>
-    <p class="text-sm text-gray-600 mb-4">{project.category}</p>
-    <p>{project.description}</p>
-    <p class="mt-2 text-sm text-gray-600">{project.languages.join(', ')}</p>
-    <p class="text-sm">{project.bottomText}</p>
+<li class="group my-4 overflow-hidden border hover:border-gray-400 rounded-xl">
+  <a href={project.url} class="block rounded-xl">
+    <div class="p-4">
+      <h3 class="font-bold text-xl mb-2">{project.name}</h3>
+      <p class="text-sm text-gray-600 mb-4">{project.category}</p>
+      <p>{project.description}</p>
+      <p class="mt-2 text-sm text-gray-600">{project.languages.join(', ')}</p>
+    </div>
+    <footer class="flex justify-end items-center bg-gray-200">
+      <div class="m-3 flex">
+        <div class="flex items-center px-4">
+          <p class="text-sm">{project.bottomText}</p>
+        </div>
+        <div class="flex place-items-center w-6">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="relative left-0 group-hover:left-1 size-4 transition-all duration-100">
+            <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+          </svg>
+        </div>
+      </div>
+    </footer>
   </a>
 </li>
