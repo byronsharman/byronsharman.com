37d4cd69aafb8321792cf0a7613cc470acb016db
b-sharman <byron.n.sharman@gmail.com>
2025-01-24 10:12:58 -0700

    fix "Recent Posts" alignment on blog page

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -47,7 +47,7 @@
     <hr class="size-full border-gray-300 my-10"/>
 
     <h2 class="w-full heading2">Recent Posts</h2>
-      <ul class="grid grid-flow-row grid-cols-1 md:grid-cols-2 gap-4">
+      <ul class="grid grid-flow-row grid-cols-1 md:grid-cols-2 w-full gap-4">
         {#each Object.values(data.blogs) as blog}
           <BlogCard blog={blog} />
         {/each}
