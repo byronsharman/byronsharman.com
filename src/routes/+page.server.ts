a032cc38b3e991e6be0beb4f174b49082c4f0d7f
b-sharman <byron.n.sharman@gmail.com>
2025-02-12 13:44:55 -0700

    TypeScript improvements

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,17 +1,17 @@
-import type { PageLoad } from './$types';
+import type { PageServerLoad } from './$types';
 
 import { blogJsonToObject, getBlogsAsJson } from '$lib/blogUtils.server';
 import { getProjects } from '$lib/getProjects.server';
 
-export const load: PageLoad = async (p) => {
+export const load: PageServerLoad = async ({ fetch }) => {
   let retval: any = {};
 
   // blog data
-  retval.blogs = await getBlogsAsJson(p.fetch)
+  retval.blogs = await getBlogsAsJson(fetch)
     .then((obj: object) => blogJsonToObject(obj))
     .catch((err: Error) => console.error(err));
 
-  retval.projects = await getProjects(p);
+  retval.projects = await getProjects(fetch);
 
   return retval;
 };
