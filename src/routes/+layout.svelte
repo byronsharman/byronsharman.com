0f18fc3a036ea5a1050abdacaca577a87fe05e98
byronsharman <byron.n.sharman@gmail.com>
2025-05-22 00:19:04 -0600

    add a <link rel="canonical" ... />
    Requires the existence of a new PUBLIC_BASE_URL environment variable.
    Seems like that's more appropriate than using Vite's environment
    variables anyway.

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang='ts'>
+import { page } from "$app/state";
+import { PUBLIC_BASE_URL } from "$env/static/public";
+
+const canonicalUrl = $derived(
+  new URL(page.url.pathname, PUBLIC_BASE_URL).toString(),
+);
+
+let { children } = $props();
+</script>
+
+<svelte:head>
+  <link rel="canonical" href={canonicalUrl} />
+</svelte:head>
+
+{@render children()}
