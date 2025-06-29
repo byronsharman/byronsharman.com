c5c692c82bee8b2d5caf87df68dfa034cbd6d1ab
b-sharman <byron.n.sharman@gmail.com>
2025-06-29 17:09:14 -0600

    make social links icons; scaffold project page
    too lazy to do git add --patch and make two commits :P

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,13 @@
+<script lang='ts'>
+import ProjectCard from "$lib/projectCard.svelte";
+import type { PageProps } from "./$types";
+
+let { data }: PageProps = $props();
+</script>
+
+<h1>Projects</h1>
+<ul class="space-y-4">
+  {#each data.projects as project}
+    <ProjectCard {project} />
+  {/each}
+</ul>
