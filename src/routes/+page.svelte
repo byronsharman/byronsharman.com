9ce764d632b6c1f4f923124d0f0c80c47bd844ec
b-sharman <byron.n.sharman@gmail.com>
2025-06-30 11:37:43 -0600

    improve footer contrast

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -86,7 +86,7 @@ let { data }: PageProps = $props();
   {/each}
 </section>
 
-<footer class="flex flex-row justify-between mt-8 lg:mt-16 text-gray-500 text-xs">
+<footer class="flex flex-row justify-between mt-8 lg:mt-16 text-gray-600 text-xs">
   <a aria-label="previous" href="https://grantlemons.com/webring/prev" class="flex flex-col justify-center">
     <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M4.205 8.72805L12.205 3.72805C13.2041 3.10363 14.5 3.82189 14.5 5.00004V15C14.5 16.1782 13.2041 16.8965 12.205 16.272L4.205 11.272C3.265 10.6845 3.265 9.31555 4.205 8.72805Z" />
