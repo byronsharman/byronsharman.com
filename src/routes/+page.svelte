d5b3084b5c9e895946ade116dde11b621cea6788
b-sharman <byron.n.sharman@gmail.com>
2025-07-25 13:02:28 -0600

    animate explore projects link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -76,9 +76,9 @@ const { data }: PageProps = $props();
     both physically and conceptually.
   </p>
 
-  <a href="/projects" class="my-std lg:my-lg underline text-fg-primary dark:text-fg-primary-dark">
+  <a href="/projects" class="my-std lg:my-lg border-b text-fg-primary dark:text-fg-primary-dark group">
     Explore my projects
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 inline">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" class="size-4 inline motion-safe:transition motion-safe:group-hover:translate-x-0.5">
       <path fill-rule="evenodd" d="M2 8a.75.75 0 0 1 .75-.75h8.69L8.22 4.03a.75.75 0 0 1 1.06-1.06l4.5 4.5a.75.75 0 0 1 0 1.06l-4.5 4.5a.75.75 0 0 1-1.06-1.06l3.22-3.22H2.75A.75.75 0 0 1 2 8Z" clip-rule="evenodd" />
     </svg>
   </a>
