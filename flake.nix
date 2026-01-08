1c3a1bf7d376d3ca003f1e30e31d3ae0ae91ef89
byronsharman <byron.n.sharman@gmail.com>
2026-01-08 10:47:48 -0700

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import (inputs.nixpkgs) { inherit system; });
+      in {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            nodejs_25
+          ];
+        };
+      }
+    );
+}
