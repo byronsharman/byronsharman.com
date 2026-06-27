e328de8dc0882b6405d6f331bc76fc754e8bb7b3
byronsharman <byron.n.sharman@gmail.com>
2026-06-26 20:58:34 -0600

    improve logging when GitHub API calls fail

--- src/routes/experience/+page.server.ts
+++ src/routes/experience/+page.server.ts
@@ -1,14 +1,7 @@
 import type { PageServerLoad } from "./$types";
 
 import { getExperience } from "$lib/server/getExperience";
-import type { Experience } from "$lib/types";
 
 export const load: PageServerLoad = async ({ fetch }) => {
-  let experiences: Experience[] = [];
-  try {
-    experiences = await getExperience(fetch);
-  } catch (e: unknown) {
-    console.error(e);
-  }
-  return { experiences };
+  return { experiences: await getExperience(fetch) };
 };
